f67b329fef59b90b23f5f1a2d4e4869b5d89e216
linfordsmith <las546@drexel.edu>
2019-06-27 18:29:32 -0400

    adding this to Module01

---
+++ Module01/Hello_World_App_Linford.py
@@ -0,0 +1,11 @@
+print('--------------')
+print('HELLO APP')
+print('--------------')
+user_text=input('What is your name human     ')
+greeting= input('Well what do you want '+ user_text)
+print('What are you going to do about it')
+
+
+
+
+
